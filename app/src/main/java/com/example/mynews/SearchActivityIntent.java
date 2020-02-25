12ac95ca002078456622f7b0dffd73274b248546
Cedxx <cedmsm@hotmail.com>
2020-02-25 13:13:09 +0100

    added DatePickerFragment that will be called in the SearchActivityIntent in order to display date selection for the research.
    Added the showDatePickerDialog method in the SearchActivityIntent.
    
    Added the corrected variable in XML file

--- app/src/main/java/com/example/mynews/SearchActivityIntent.java
+++ app/src/main/java/com/example/mynews/SearchActivityIntent.java
@@ -1,8 +1,10 @@
 package com.example.mynews;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 public class SearchActivityIntent extends AppCompatActivity {
 
@@ -13,4 +15,10 @@ public class SearchActivityIntent extends AppCompatActivity {
         // Linking the elements in the layout to Java code
         setContentView(R.layout.search_main);
     }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
 }
